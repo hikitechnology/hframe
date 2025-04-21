4f236a8905969e16aeb9433a06c9c266450ec73c
hikipilling <hikipilled@proton.me>
2025-04-21 16:00:15 -0400

    configured library build

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { dirname, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
@@ -12,4 +13,5 @@ export default defineConfig({
       fileName: "hframe",
     },
   },
+  plugins: [dts({ rollupTypes: true, include: ["lib"] })],
 });
