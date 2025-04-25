3a9086488db9cf91faffde385452e10c6925fba7
hikipilling <hikipilled@proton.me>
2025-04-25 12:35:53 -0400

    prevent 2 resizes at once

--- lib/ui/abstract/resize-layout.ts
+++ lib/ui/abstract/resize-layout.ts
@@ -156,6 +156,7 @@ export abstract class ResizeLayout extends DirectionalLayout {
           this.growAllocation(nextAllocation, -resizeDelta);
         }
       }
+      context.pauseInteraction();
     }
   }
 
